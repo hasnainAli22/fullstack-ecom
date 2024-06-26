954552c7852d9ee1abda260c3509f0ae92517063
hasnain22 <our.hasnain22@gmail.com>
2024-06-26 21:18:10 +0500

    Successfull Merge

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -91,12 +91,17 @@ export const fetchUserProfile = async (token: string): Promise<any> => {
   });
   return response.data;
 };
-export const fetchProducts = async (params: FetchProductsParams): Promise<Product[]> => {
-    const response = await axios.get(`${API_URL}/products/products/`, { params });
+export const fetchProducts = async (params?: FetchProductsParams): Promise<Product[]> => {
+    const response = await axios.get(`${API_URL}/products/products`);
 return response.data;
 };
 
 export const fetchCategories = async (): Promise<Category[]> => {
-    const response = await axios.get(`${API_URL}/products/categories/`);
+    const response = await axios.get(`${API_URL}/products/categories`);
     return response.data;
 };
+
+export const fetchProductsById = async(id: number): Promise<Product> => {
+  const response = await axios.get(`${API_URL}/products/products/${id}`)
+  return response.data;
+}
