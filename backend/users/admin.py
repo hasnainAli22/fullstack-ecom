8111b9c70e99596ea8d68b19e550192f3d13b358
iamluckycheema <luckycheema002337@gmail.com>
2024-06-28 02:08:10 +0500

    requirments.txt added

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserAccount
 
 # Register your models here.
+@admin.register(UserAccount)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser')
+    search_fields = ('name',)
